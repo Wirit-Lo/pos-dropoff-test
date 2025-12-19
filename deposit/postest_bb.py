2ac271cd43e89a27d8de47e98416b7dfaa9be58c
Wirit <oak.wiritpon@gmail.com>
2025-12-19 22:21:44 +0700

    update test pos -v1.2.7

--- deposit/postest_bb.py
+++ deposit/postest_bb.py
@@ -724,8 +724,8 @@ def run_smart_scenario(main_window, config):
         time.sleep(0.5)
 
     # เลือกบริการขนส่ง (ใช้ ShippingService_2579 สำหรับ EMS สำเร็จรูป)
-    wait_until_id_appears(main_window, "ShippingService_363163", timeout=15)
-    if find_and_click_with_rotate_logic(main_window, "ShippingService_363163"):
+    wait_until_id_appears(main_window, "ShippingService_363235", timeout=15)
+    if find_and_click_with_rotate_logic(main_window, "ShippingService_363235"):
         main_window.type_keys("{ENTER}")
     
     time.sleep(1)
