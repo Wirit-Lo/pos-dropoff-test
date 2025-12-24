6f13cab27b2d64f141433c6d3153ecd12148df8b
Wirit <oak.wiritpon@gmail.com>
2025-12-24 11:06:22 +0700

    test fruit1 -v1.0.2

--- deposit/fruit1.py
+++ deposit/fruit1.py
@@ -679,8 +679,8 @@ def run_smart_scenario(main_window, config):
         time.sleep(0.5)
     # =========================================================
 
-    wait_until_id_appears(main_window, "ShippingService_363163", timeout=15)
-    if find_and_click_with_rotate_logic(main_window, "ShippingService_363163"):
+    wait_until_id_appears(main_window, "ShippingService_363200", timeout=15)
+    if find_and_click_with_rotate_logic(main_window, "ShippingService_363200"):
         main_window.type_keys("{ENTER}")
 
     if add_insurance_flag.lower() in ['true', 'yes']:
